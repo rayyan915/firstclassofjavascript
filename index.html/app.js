37b594fc1b34ef3439f0ea8f8fb084c63268e273
Hassan Ali <hassanali20242025@gmail.com>
2024-12-16 15:02:51 +0500

    Add files via upload

---
+++ index.html/app.js
@@ -0,0 +1,15 @@
+// chapter 1
+alert("Explore My Website");
+
+// chapter 2
+var email = "hassanali@gmail.com";
+alert(email);
+
+var write = "<h1>Trust Me I Am A Programmer</h1>";
+document.write("<center>"+write+"</center>");
+
+// chapter 3
+var string = "I Got\n ";
+var num = 93.82 ;
+var string2 = "% \nMarks in Board";
+alert(string + num + string2);
